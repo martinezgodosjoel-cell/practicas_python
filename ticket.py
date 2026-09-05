44ada9192206346e6edf5b391a6bc69cf686df61
martinezgodosjoel-cell <martinezgodosjoel@gmail.com>
2026-09-04 23:07:20 -0600

    Actualizar ticket.py

--- ticket.py
+++ ticket.py
@@ -1,4 +1,4 @@
-Nombre = input("Nombre:")
-Compra = input("Producto:")
-Valor = float(input("Precio:"))
-print (f"---Ticket-- \nCliente: {Nombre.strip().title()} \nProducto: {Compra.upper().strip()} \nTotal: {Valor:.2f} \nGracias por tu compra!")
+nombre = input("Nombre:")
+compra = input("Producto:")
+valor = float(input("Precio:"))
+print (f"---Ticket-- \nCliente: {nombre.strip().title()} \nProducto: {compra.upper().strip()} \nTotal: {valor:.2f} \nGracias por tu compra!")
